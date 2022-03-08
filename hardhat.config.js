cf12c0c3a69fd69a7187ee1e194a02ae764b1001
Saif ur Rehman <saifch420@gmail.com>
2022-03-09 03:03:31 +0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,8 +3,8 @@
  */
 require('@nomiclabs/hardhat-waffle');
 
-const INFURA_URL = 'https://rinkeby.infura.io/v3/b71c84a9ca754cd99703d212ae4287bc';
-const PRIVATE_KEY = 'd55878384be5aacc497db207aec296a35d29457d6bb49c7dfbb2dfee054a3243';
+const INFURA_URL = '';
+const PRIVATE_KEY = '';
 
 module.exports = {
   solidity: "0.8.0",
